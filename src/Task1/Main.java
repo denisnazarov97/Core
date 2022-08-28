a624e841d6d41bf0f941990a11b90b38848813c7
denisnazarov97 <ricki19967@yandex.ru>
2022-08-28 20:25:05 +0300

    commit

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,9 +1,42 @@
 package Task1;
 
-public class Main {
-    public static void main(String[] args) {
-        double [][] array1 = new double[5][5];
-        double a = Math.random()+2;
+import java.util.Random;
 
+public class Main {
+    public static void main (String[] args){
+        double[][] a = new double[5][5];
+        double max, min;
+        double maxa = a [0][0];
+        double mina = a[0][0];
+        double avg = 0;
+        Random rnd = new Random();
+        for (int i=0;i < a.length;i++) {
+            for (int j=0;j < a[i].length;j++) {
+                a[i][j]=rnd.nextInt(10);
+            }
+        }
+        for (int i=0;i < a.length;i++,System.out.println()) {
+            for (int j=0;j < a[i].length;j++) {
+                System.out.print(a[i][j]+" ");
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length; j++) {
+                if (maxa < a[i][j]){
+                    maxa = a[i][j];
+                }
+                else if(mina > a[i][j]){
+                    mina = a[i][j];
+                }
+                avg += a[i][j]/(a.length*a.length);
+            }
+        }
+        System.out.println("Максимальный элемент двумерного массива " + maxa);
+        System.out.println("Минимальный элемент двумерного массива " + mina);
+        System.out.println("Cреднее значение двумерного массива:" + avg);
     }
 }
+
+
+
